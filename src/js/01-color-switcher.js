449b75f27f29ae6502587f0b2e7abf8946459812
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-11 18:50:01 +0100

    task 1 buttons disable correction

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,24 @@
 const startBtn = document.querySelector('.data-start');
 const stopBtn = document.querySelector('.data-stop');
 let timerId = null;
+let themeChanging = false;
 
 startBtn.addEventListener('click', () => {
-  timerId = setInterval(() => {
-    const randomColor = getRandomHexColor();
-    document.querySelector('body').style.backgroundColor = randomColor;
-  }, 1000);
+  if (!themeChanging) {
+    themeChanging = true;
+    startBtn.disabled = true; // set disabled
+
+    timerId = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      document.querySelector('body').style.backgroundColor = randomColor;
+    }, 1000);
+  }
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
+  themeChanging = false;
+  startBtn.disabled = false; // clear disabled
 });
 
 function getRandomHexColor() {
