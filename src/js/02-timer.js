f0f0a5e8350420fff14a27bc53718f86c5b2c113
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-12 19:32:17 +0100

    alert position centered

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -35,7 +35,7 @@ document.querySelector('[data-start]').addEventListener('click', () => {
       iziToast.show({
         title: 'Timer Expired!',
         message: '',
-        position: 'topRight',
+        position: 'topCenter',
         timeout: 3000,
         backgroundColor: '#ff5733',
         color: 'white',
@@ -74,7 +74,7 @@ flatpickr('#datetime-picker', {
       iziToast.error({
         title: 'Error',
         message: 'Please choose a date in the future',
-        position: 'topRight',
+        position: 'topCenter',
         timeout: 3000,
         backgroundColor: '#ff5733',
         color: 'white',
