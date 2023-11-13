b52ab7117233c046985f2bd11e67546c58e051c3
Borys-Grinberg <borys.grinberg@gmail.com>
2023-11-13 06:19:01 +0100

    Last js3 change

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,47 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.form');
+// 03-promises.js
 
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
+const form = document.querySelector('.form');
 
-    const formData = new FormData(form);
-    const firstDelay = parseInt(formData.get('delay'));
-    const delayStep = parseInt(formData.get('step'));
-    const amount = parseInt(formData.get('amount'));
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
 
-    for (let i = 0; i < amount; i++) {
-      createPromise(i + 1, firstDelay + i * delayStep)
-        .then(({ position, delay }) => {
-          iziToast.success({
-            title: 'Fulfilled',
-            message: `Promise ${position} fulfilled in ${delay}ms`,
-            position: 'topRight',
-          });
-        })
-        .catch(({ position, delay }) => {
-          iziToast.error({
-            title: 'Rejected',
-            message: `Promise ${position} rejected in ${delay}ms`,
-            position: 'topRight',
-          });
+  const formData = new FormData(form);
+  const firstDelay = parseInt(formData.get('delay'));
+  const delayStep = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, firstDelay + i * delayStep)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: 'Fulfilled',
+          message: `Promise ${position} fulfilled in ${delay}ms`,
+          position: 'topRight',
         });
-    }
-  });
-
-  function createPromise(position, delay) {
-    return new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
-
-      setTimeout(() => {
-        if (shouldResolve) {
-          resolve({ position, delay });
-        } else {
-          reject({ position, delay });
-        }
-      }, delay);
-    });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: 'Rejected',
+          message: `Promise ${position} rejected in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
   }
 });
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
